770871f81e72af5f299f785b9739fe1282ca0606
Kevin McCarthy <kjmagic13@gmail.com>
2022-12-17 10:53:18 -0500

    remove baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/ai-musings/',
-  },
-
   modules: [
     //
     '@nuxtjs/tailwindcss',
