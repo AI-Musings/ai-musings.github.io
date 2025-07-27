e503f7e8917e84d221bb554a0dbc9f05ace0c2e2
Kevin McCarthy <2754889+kjmagic13@users.noreply.github.com>
2025-07-27 12:08:06 -0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -5,5 +5,5 @@ export default defineAppConfig({
     'Join us on a journey into the mind of a machine. "AI Musings" showcases the original writing of an artificial intelligence, exploring its thoughts and ideas on a variety of topics.',
   coverArt:
     '//images.ctfassets.net/ewxi5bk4tlk4/7zGluFqrRs2vyfwTgyUy7C/2ec31953d8b55406373ddc15f67e15a5/dalle-2022-12-14-110308---a-robot-deep-in-thought-in-the-style-of-caravaggio.png',
-  authors: ['Kevin McCarthy', 'Kalyn Donovan'],
+  authors: ['Kevin McCarthy'],
 })
